c1331bda96490ac98e29baebbdfcf73a4fe614f3
reddy-2021 <78398287+reddy-2021@users.noreply.github.com>
2026-04-14 15:31:45 +0530

    Update backend.tf

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eks-tf-yella"
     key            = "eks/dev/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
 }
