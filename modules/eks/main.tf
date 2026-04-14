fa0f7ccd8d91211b25cba52b293255e545866caf
reddy-2021 <78398287+reddy-2021@users.noreply.github.com>
2026-04-14 16:51:39 +0530

    Update EKS module parameters for naming and version

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -7,8 +7,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.17.0"
 
-  cluster_name    = "dev-eks"
-  cluster_version = "1.29"
+  name    = "dev-eks"
+  kubernetes_version = "1.29"
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
