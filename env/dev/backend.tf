90d9ca71621bfdbf02172d956083353c43f4a132
reddy-2021 <78398287+reddy-2021@users.noreply.github.com>
2026-04-14 15:29:39 +0530

    Change S3 bucket name for Terraform backend

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-tf-state-bucket"
+    bucket         = "eks-tf-yella"
     key            = "eks/dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
