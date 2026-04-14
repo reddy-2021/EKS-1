4d891f3bdb234ae04577e4ee8edc943a557d368f
reddy-2021 <78398287+reddy-2021@users.noreply.github.com>
2026-04-14 15:45:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"   # ✅ MUST
+    }
+  }
+}
+
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
